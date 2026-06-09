1ea601a00ac12a0f0ef712e74be4a5911a789097
sadrda <kofler_a@yahoo.de>
2026-06-09 14:51:32 -0700

    feat: add model

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-  return <></>
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, OrbitControls } from "@react-three/drei";
+
+type RoadProps = {
+  position?: [number, number, number];
+  rotation?: [number, number, number];
+  scale?: [number, number, number] | number;
+};
+
+function Road(props: RoadProps) {
+  const { scene } = useGLTF("/road.glb");
+  return <primitive object={scene} {...props} />;
 }
 
-export default App
+function App() {
+  return (
+    <Canvas style={{ width: "100vw", height: "100vh" }}>
+      <ambientLight intensity={1} />
+      <directionalLight position={[5, 10, 5]} intensity={2} />
+      <Road position={[0, -160, 0]} />
+      <OrbitControls />
+    </Canvas>
+  );
+}
+
+export default App;
